3bb8c3e195597dae991526277cb85f828c5db69f
EmaleeSoto <emaleesoto@gmail.com>
2022-05-17 12:52:44 -0400

    Created App class, imported student data to Index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from "react";
+import "./App.css";
+import index from "./index";
 
-function App() {
-  return (
-    <div>
-      <h1>Student Dashboard</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <h1>Student Dashboard</h1>
+        <div></div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import data from "./data/data";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App data={data} />
   </React.StrictMode>
 );
