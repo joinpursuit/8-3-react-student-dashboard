f08fdcea8bc590ed502639ac3dfcb281b13118f1
Edwin Codrington <edwincodrington@gmail.com>
2022-05-18 12:18:52 -0400

    Base app skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React from "react";
+import "./App.css";
+import CohortDirectory from "./Components/CohortDirectory";
+import StudentDirectory from "./Components/StudentDirectory";
 
-function App() {
-  return (
-    <div>
-      <h1>Student Dashboard</h1>
-    </div>
-  );
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			currentCohort: "default",
+		};
+	}
+	render() {
+		return (
+			<section>
+				<div className="header">
+					<h1 className="title">Student Dashboard</h1>
+				</div>
+				<main>
+					<CohortDirectory info={this.props.object} />
+					<StudentDirectory info={this.props.object} />
+				</main>
+			</section>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Data from "./data/data.json";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App object={Data} />
+	</React.StrictMode>
 );
