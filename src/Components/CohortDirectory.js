96ffdbd4959195e750dd71beff04b469ceb9d0db
Edwin Codrington <edwincodrington@gmail.com>
2022-05-18 12:17:25 -0400

    Component files

---
+++ src/Components/CohortDirectory.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const CohortDirectory = (props) => {
+	const { info } = props;
+	let cohortList = []; //TODO: sort this at some point
+	info.forEach((item) => {
+		if (!cohortList.includes(item.cohort.cohortCode)) {
+			cohortList.push(item.cohort.cohortCode);
+		}
+	});
+	return (
+		<ul className="directory">
+			<h4>All Students</h4>
+			{cohortList.map((cohort) => {
+				return (
+					<li
+						id={cohort}
+						key={cohort}
+						onClick={console.log(`Change this.state.currentCohort to ${cohort}`)}
+					>
+						<a href="">{cohort}</a>
+					</li>
+				);
+			})}
+		</ul>
+	);
+};
+
+export default CohortDirectory;
---
+++ src/Components/StudentDirectory.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Student from "./Student";
+
+const StudentDirectory = (props) => {
+	return <div>hi</div>;
+};
+
+export default StudentDirectory;
