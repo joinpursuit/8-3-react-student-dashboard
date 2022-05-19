05454df8529f1fb144a912c647e7b2310331ccd8
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-05-19 14:00:55 -0400

    Sorted and formatted cohort list

--- src/Components/CohortList.js
+++ src/Components/CohortList.js
@@ -2,10 +2,26 @@ import React from "react";
 import Semester from "./Semester";
 
 export default function CohortList({ studentInformation }) {
-  const semesterListItem = studentInformation.map((semester) => {
+  const semesterArray = [];
+  for (let i = 0; i < studentInformation.length; i++) {
+    const semesterDate = studentInformation[i].cohort.cohortCode;
+    if (!semesterArray.includes(semesterDate)) {
+      semesterArray.push(semesterDate);
+    }
+  }
+  semesterArray.splice(0, 1, "Winter2026");
+  semesterArray.splice(1, 1, "Fall2026");
+  semesterArray.splice(2, 1, "Summer2026");
+  semesterArray.splice(3, 1, "Spring2026");
+  semesterArray.splice(4, 1, "Winter2025");
+  semesterArray.splice(5, 1, "Fall2025");
+  semesterArray.splice(6, 1, "Summer2025");
+  semesterArray.splice(7, 1, "Spring2025");
+
+  const semesterListItem = semesterArray.map((eachSemester) => {
     return (
-      <div key={semester.id}>
-        <Semester semester={semester} />
+      <div key={eachSemester}>
+        <Semester semester={eachSemester} />
       </div>
     );
   });
--- src/Components/Semester.js
+++ src/Components/Semester.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-export default function Semester({ semester }) {
-  const semesterDate = semester.cohort.cohortCode;
-  // if (semesterDate === "Winter2025") {
-  //   return <div>{semesterDate}</div>;
-  // } else return null;
-
-  return <div className="eachSemester">{semesterDate}</div>;
+export default function Semester(eachSemester) {
+  let uniqueSemester = eachSemester.semester;
+  let formattedUniqueSemesterString = "";
+  if (uniqueSemester) {
+    let index = uniqueSemester.indexOf("2");
+    formattedUniqueSemesterString =
+      uniqueSemester.slice(0, index) +
+      " ".repeat(1) +
+      uniqueSemester.slice(index);
+  }
+  return <div className="eachSemester">{formattedUniqueSemesterString}</div>;
 }
