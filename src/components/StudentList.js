70958a888d0287a392745108afa8fb45560785a6
Charlie Moran <Carlosamoran18@gmail.com>
2022-05-19 10:32:28 -0400

    student stuff

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,1 +1,16 @@
 import React from "react";
+import Student from "./Student";
+
+class StudentList extends React.Component {
+    render() {
+        return (
+            <div className="studentList">
+                <h2>All Students</h2>
+                <h3>Total Students: 420</h3>
+                <Student />
+            </div>
+        )
+    }
+}
+
+export default StudentList;
