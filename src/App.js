76803721a4c0869c174673b8e7c9a3cf40ba9012
bowersjuan <bowersjuan@gmail.com>
2022-05-19 04:23:36 -0400

    Update App,js as Class Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-function App() {
-  return (
-    <div>
-      <header className="header">
-        <h1>Student Dashboard</h1>
-      </header>
+import react from "react";
+import CohortList from "./Components/CohortList";
+import StudentList from "./Components/StudentList";
+
+class App extends react.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
       <div>
-        <CohortList />
-        <StudentList />
+        <header className="header">
+          <h1>Student Dashboard</h1>
+        </header>
+        <main className="main">
+          <CohortList />
+          <StudentList />
+        </main>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
