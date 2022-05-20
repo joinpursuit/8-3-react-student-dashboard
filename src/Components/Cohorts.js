6ef3b99f3f931ca9c294fe608ab9ee81cc7c5943
Andres Varon <anvaron@gmail.com>
2022-05-20 09:41:29 -0400

    Cohort list component - The cohorts are organized by date, either in ascending or descending order: fixed

--- src/Components/Cohorts.js
+++ src/Components/Cohorts.js
@@ -10,24 +10,42 @@ class Cohorts extends React.Component {
 
   render() {
     // >> Getting the cohorts data 
+    // >> The cohorts are organized by date, either in ascending or descending order. "All Students" should remain at the top.
+    let cohortList = [];
+    //let newArray2 = ['All': 'Students'];
+    
     const filteredList = new Set(
       (this.props.data).map(e => e.cohort['cohortCode'])
     )
-    const cohortList = ['*'].concat(Array.from(filteredList));
+    const cohortList2 = ['*'].concat(Array.from(filteredList));
+    cohortList2.map((cohort, index) => {
+      // >> Creating a new array with objects {semester: value, year: value}
+      cohortList.push({['semester']: cohort.slice(0, -4), ['year']: cohort.slice(-4)});
+    })
+
+    cohortList.sort((a, b) => {return a.year - b.year;});
 
     return(
       <section>
         <h2>Choose a class by Start Date</h2>
         <ul>
-          {cohortList.map((cohort) => {
+          {/* {cohortList.map((cohort) => {
             if(cohort === '*') {
               return <li onClick={() => this.props.getCohort('*')}>All Students</li> 
             }
             else {
               return <li onClick={() => this.props.getCohort(cohort)}>{`${cohort.slice(0, -4)} ${cohort.slice(-4)}`}</li>
             }
-          })}
+          })} */}
           
+          {cohortList.map((cohort) => {
+            if(cohort.year === '*') {
+              return <li onClick={() => this.props.getCohort('*')}>All Students</li> 
+            }
+            else {
+              return <li onClick={() => this.props.getCohort(`${cohort.semester}${cohort.year}`)}>{`${cohort.semester} ${cohort.year}`}</li>
+            }
+          })}
         </ul>
       </section>
     )
