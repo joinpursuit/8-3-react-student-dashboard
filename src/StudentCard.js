c6ed733412563f68ada76d7b797fa03c1417fe26
EmaleeSoto <emaleesoto@gmail.com>
2022-05-21 15:36:14 -0400

    StudentCard is no longer a class component, and some minor CSS changes

--- src/StudentCard.js
+++ src/StudentCard.js
@@ -1,20 +1,50 @@
-import React from "react";
 import "./StudentCard.css";
 
-class StudentCard extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      extrasVisible: false,
-    };
+const formatBirthdayText = (birthday) => {
+  const newBirthday = birthday.split("/");
+  let month = "";
+  switch (newBirthday[0]) {
+    case "1":
+      month = "January";
+      break;
+    case "2":
+      month = "February";
+      break;
+    case "3":
+      month = "March";
+      break;
+    case "4":
+      month = "April";
+      break;
+    case "5":
+      month = "May";
+      break;
+    case "6":
+      month = "June";
+      break;
+    case "7":
+      month = "July";
+      break;
+    case "8":
+      month = "August";
+      break;
+    case "9":
+      month = "September";
+      break;
+    case "10":
+      month = "October";
+      break;
+    case "11":
+      month = "November";
+      break;
+    case "12":
+      month = "December";
+      break;
   }
+  return `${month} ${newBirthday[1]}, ${newBirthday[2]} `;
+};
 
-  setVisibility = (isVisible) => {
-    this.setState({
-      extrasVisible: isVisible,
-    });
-  };
-
+const StudentCard = ({ student }) => {
   // showHidden = () => {
   //   document.querySelectorAll(".truncated").forEach((current) => {
   //     current.addEventListener("click", () => {
@@ -23,79 +53,21 @@ class StudentCard extends React.Component {
   //   });
   // };
 
-  formatBirthdayText = (birthday) => {
-    const newBirthday = birthday.split("/");
-    let month = "";
-    switch (newBirthday[0]) {
-      case "1":
-        month = "January";
-        break;
-      case "2":
-        month = "February";
-        break;
-      case "3":
-        month = "March";
-        break;
-      case "4":
-        month = "April";
-        break;
-      case "5":
-        month = "May";
-        break;
-      case "6":
-        month = "June";
-        break;
-      case "7":
-        month = "July";
-        break;
-      case "8":
-        month = "August";
-        break;
-      case "9":
-        month = "September";
-        break;
-      case "10":
-        month = "October";
-        break;
-      case "11":
-        month = "November";
-        break;
-      case "12":
-        month = "December";
-        break;
-    }
-    return `${month} ${newBirthday[1]}, ${newBirthday[2]} `;
-  };
-
-  createStudentCard = ({ student }) => {
-    const { names, profilePhoto, username, dob } = student;
-    return (
-      <div className="card">
-        <h4>{`${names.preferredName} ${names.middleName} ${names.surname}`}</h4>
-        <p>{username}</p>
-        <img src={profilePhoto} alt={names.preferredName} />
-        <div className="birthday">
-          <p className="green left">Birthday: </p>
-          <p className="dob right">{this.formatBirthdayText(dob)}</p>
-        </div>
-        <div className="truncated">
-          <p
-            className="green show-more"
-            onClick={() => this.setVisibility(!this.state.extrasVisible)}
-          >
-            Show more...
-          </p>
-          <p>CodeWars</p>
-          <p>Scores</p>
-          <p>Certifications</p>
-        </div>
+  const { names, profilePhoto, username, dob } = student;
+  return (
+    <div className="card">
+      <h4>{`${names.preferredName} ${names.middleName} ${names.surname}`}</h4>
+      <p>{username}</p>
+      <img src={profilePhoto} alt={names.preferredName} />
+      <div className="birthday">
+        <p className="green left">Birthday: </p>
+        <p className="dob right">{formatBirthdayText(dob)}</p>
       </div>
-    );
-  };
-  render() {
-    const { student } = this.props;
-    return <div>{this.createStudentCard({ student })}</div>;
-  }
-}
+      <div>
+        <p className="green show-more">Show more...</p>
+      </div>
+    </div>
+  );
+};
 
 export default StudentCard;
