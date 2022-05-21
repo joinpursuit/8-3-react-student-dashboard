63ec0105634eaa315650181460adeb0209560b8a
EmaleeSoto <emaleesoto@gmail.com>
2022-05-21 13:44:30 -0400

    Formatted birthdays, set up show more functionality, not yet complete

--- src/StudentCard.js
+++ src/StudentCard.js
@@ -1,33 +1,101 @@
+import React from "react";
 import "./StudentCard.css";
 
-const showHidden = () => {
-  document.querySelectorAll(".truncated").forEach((current) => {
-    current.addEventListener(
-      "click",
-      (e) => {
-        current.classList.remove("truncated");
-      },
-      false
-    );
-  });
-};
+class StudentCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      extrasVisible: false,
+    };
+  }
 
-const StudentCard = ({ student }) => {
-  const { names, profilePhoto, username, dob } = student;
-  return (
-    <div className="card">
-      <h5>{`${names.preferredName} ${names.middleName} ${names.surname}`}</h5>
-      <p>{username}</p>
-      <img src={profilePhoto} alt={names.preferredName} />
-      <p>{`Birthday: ${dob}`}</p>
-      <div className="truncated">
-        <p onClick={() => showHidden()}>Show More..</p>
-        <p>CodeWars</p>
-        <p>Scores</p>
-        <p>Certifications</p>
+  setVisibility = (isVisible) => {
+    this.setState({
+      extrasVisible: isVisible,
+    });
+  };
+
+  // showHidden = () => {
+  //   document.querySelectorAll(".truncated").forEach((current) => {
+  //     current.addEventListener("click", () => {
+  //       studentCard.setExtrasVisibility(!studentCard.isVisible);
+  //     });
+  //   });
+  // };
+
+  formatBirthdayText = (birthday) => {
+    const newBirthday = birthday.split("/");
+    let month = "";
+    switch (newBirthday[0]) {
+      case "1":
+        month = "January";
+        break;
+      case "2":
+        month = "February";
+        break;
+      case "3":
+        month = "March";
+        break;
+      case "4":
+        month = "April";
+        break;
+      case "5":
+        month = "May";
+        break;
+      case "6":
+        month = "June";
+        break;
+      case "7":
+        month = "July";
+        break;
+      case "8":
+        month = "August";
+        break;
+      case "9":
+        month = "September";
+        break;
+      case "10":
+        month = "October";
+        break;
+      case "11":
+        month = "November";
+        break;
+      case "12":
+        month = "December";
+        break;
+    }
+    return `${month} ${newBirthday[1]}, ${newBirthday[2]} `;
+  };
+
+  createStudentCard = ({ student }) => {
+    const { names, profilePhoto, username, dob } = student;
+    return (
+      <div className="card">
+        <h4>{`${names.preferredName} ${names.middleName} ${names.surname}`}</h4>
+        <p>{username}</p>
+        <img src={profilePhoto} alt={names.preferredName} />
+        <div className="birthday">
+          <p className="green left">Birthday: </p>
+          <p className="dob right">{this.formatBirthdayText(dob)}</p>
+        </div>
+        <div className="truncated">
+          <p
+            className="green show-more"
+            onClick={() => this.setVisibility(!this.state.extrasVisible)}
+          >
+            Show more...
+          </p>
+          <p>CodeWars</p>
+          <p>Scores</p>
+          <p>Certifications</p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+  render() {
+    const { student } = this.props;
+    return <div>{this.createStudentCard({ student })}</div>;
+  }
+}
 
 export default StudentCard;
