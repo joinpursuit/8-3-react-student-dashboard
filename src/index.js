cc993b7f9f9baaef38b08e474a9947e7f719da68
EmaleeSoto <emaleesoto@gmail.com>
2022-05-21 14:40:53 -0400

    Index no longer passes student data array to App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import data from "./data/data";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App data={data} />
+    <App />
   </React.StrictMode>
 );
