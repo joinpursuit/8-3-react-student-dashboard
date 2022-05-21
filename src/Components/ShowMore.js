08142e2758c22645c410cb8b168740ed08519e67
Edwin Codrington <edwincodrington@gmail.com>
2022-05-20 20:29:08 -0400

    I CANNOT BE STOPPED

--- src/Components/ShowMore.js
+++ src/Components/ShowMore.js
@@ -1,11 +1,31 @@
 import React from "react";
 
-function ShowMore(props) {
-	return (
-		<div className="show-more hidden" id={props.id}>
-			<button>Show More...</button>
-		</div>
-	);
+class ShowMore extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			showMore: true,
+		};
+		//this.studentInfo = this.props;
+	}
+	// { studentInfo } = this.props;
+	extraInfo = () => {
+		this.setState({ showMore: !this.state.showMore });
+	};
+	render() {
+		return (
+			<div className="show-more" id={this.props.id}>
+				<button onClick={this.extraInfo}>
+					{this.state.showMore ? "Show More" : "Show Less"}
+				</button>
+				<div className={this.state.showMore ? "hidden" : null}>
+					<div>Codewars</div>
+					<div>Scores</div>
+					<div>Certifications</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default ShowMore;
