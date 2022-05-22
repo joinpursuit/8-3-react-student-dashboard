8699f0f4c738bf11865fec5a68d752012e9f2aa4
bowersjuan <bowersjuan@gmail.com>
2022-05-22 17:25:11 -0400

    Update StudentCard.css and StudentCard.js styling

--- src/Components/StudentCard.js
+++ src/Components/StudentCard.js
@@ -5,12 +5,15 @@ class StudentCard extends React.Component {
   render() {
     return (
       <div className="student-card">
-        <img src={this.props.pic} alt="Profile" width="100px" />
-        <div>
-          <h4>{this.props.name}</h4>
-          <p>Email: {this.props.email}</p>
+        <img id="pic" src={this.props.pic} alt="Profile" width="100px" />
+        <div id="student-profile">
+          <p>
+            <strong>{this.props.name}</strong>
+          </p>
+          <p>{this.props.email}</p>
           <p>Birthday: {this.props.birthday}</p>
-          <p>Show More...</p>
+          <br></br>
+          <p id="show-more">Show More...</p>
         </div>
       </div>
     );
