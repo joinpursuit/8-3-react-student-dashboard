5a735ea80b6d7af5a2bbc3bc2623559f50f7db6a
bowersjuan <bowersjuan@gmail.com>
2022-05-22 17:05:17 -0400

    Update CohortList.js and CohortList.css styling for All Students Button

--- src/Components/CohortList.js
+++ src/Components/CohortList.js
@@ -4,6 +4,7 @@ function CohortList(props) {
   return (
     <aside className="cohort-list-container">
       <h2>Choose a Class by Start Date</h2>
+      <button id="all-students-button">All Students</button>
       <div className="cohort-list">{props.cohortsArray}</div>
     </aside>
   );
