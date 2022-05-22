bc2194e54026db02ba1b6c104c61e213e5950470
Charlie Moran <Carlosamoran18@gmail.com>
2022-05-21 20:45:03 -0400

    student cards

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -2,11 +2,64 @@ import React from "react";
 import Student from "./Student";
 
 class StudentList extends React.Component {
+    selectCohort = (input, selectedCohort) => {
+        if(selectedCohort === "AllStudents") {
+            return input;
+        } else {
+            return input.filter((student) => {
+                return student.cohort.cohortCode === selectedCohort;
+            })
+        }
+    };
+
+    displayStudents = (
+        data,
+        selectedCohort,
+        deets,
+        deetsNstuff,
+        comments,
+        commentStuff,
+     ) => {
+        let selectedStudent = this.selectCohort(input, selectedCohort);
+
+        return selectedStudent.map((selectedStudent, index) => {
+            return (
+                <Student studentInfo={selectedStudent} key={index} 
+                         deets={deets} deetsNstuff={deetsNstuff}
+                         comments={comments} commentStuff={commentStuff} 
+                />
+            )
+        })
+    };
+
+    parseCohort = (cohort) => {
+        if(cohort === "AllStudents") return "All Students";
+        return `${cohort.substring(0, cohort.length -4)} ${cohort.substring(cohort.length -4)}`
+    };
+
     render() {
+        const {
+            data,
+            selectedCohort,
+            deets,
+            deetsNstuff,
+            comments,
+            commentStuff,
+        } = this.props
+
+        const studentCards = this.displayStudents(
+            data,
+            selectedCohort,
+            deets,
+            deetsNstuff,
+            comments,
+            commentStuff,
+        )
         return (
             <div className="studentList">
-                <h2>All Students</h2>
-                <h3>Total Students: 420</h3>
+                <h2>{this.parseCohort(selectedCohort)}</h2>
+                <h3>Total Students: {studentCards.length}</h3>
+                {studentCards}
                 <Student />
             </div>
         )
