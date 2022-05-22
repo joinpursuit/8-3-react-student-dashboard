9c318ce80d0384b44d36ede64c25d860e0ee26ac
Charlie Moran <Carlosamoran18@gmail.com>
2022-05-21 20:25:31 -0400

    Show More Button

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,14 +1,36 @@
 import React from "react";
 
 class Student extends React.Component {
+
+    showMore = (studentInfo, comments, commentStuff) => {
+        return(
+            <Details studentInfo={studentInfo} comments={comments} commentStuff={commentStuff} />
+        )
+    };
+
     render(){
+        const {studentInfo} = this.props;
+        const Birthday = studentInfo.dob;
+        const email = studentInfo.username;
+        const imgSrc = studentInfo.profilePhoto;
+        const name = `${studentInfo.names.preferredName} ${studentInfo.names.middleName[0]}, ${studentInfo.names.surname}`;
+        const {
+            comments,
+            commentStuff,
+            deets,
+            deetsNstuff,
+        } = this.props
+
         return (
             <div className="student">
                 <img src="https://fakeface.rest/thumb/view?gender=female&minimum_age=48" />
                 <p>Banana Woman</p>
                 <p>bananawoman@pursuit.org</p>
                 <p>Birthday: 6/9/420</p>
-                <button>Show More</button>
+                <button onClick={()=> deetsNstuff(studentInfo.id)}>
+                    {deets.includes(studentInfo.id) ? 'Show Less' : 'Show More'}
+                </button>
+                {deets.includes(studentInfo.id) ? this.showMore(studentInfo, comments, commentStuff) : null}
             </div>
         );
     }
