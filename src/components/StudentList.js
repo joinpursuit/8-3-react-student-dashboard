65236cd90a209cd53504106d09a16c744494d3d7
Jonathan Scheiber <jon@scheiber.me>
2022-05-23 00:37:38 -0400

    update StudentList.js

---
+++ src/components/StudentList.js
@@ -0,0 +1,15 @@
+import StudentCard from "./StudentCard";
+
+const StudentList = ({ data, title }) => (
+  <div className="all-students">
+    <h3>{title}</h3>
+    <p className="total">
+      Total Students: <span>{data.length}</span>
+    </p>
+    {data.map((student) => (
+      <StudentCard data={student} />
+    ))}
+  </div>
+);
+
+export default StudentList;
