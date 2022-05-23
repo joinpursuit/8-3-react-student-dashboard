e17d31b9aa10b9f8fe896910800ec7430b57c753
EmaleeSoto <emaleesoto@gmail.com>
2022-05-23 10:17:07 -0400

    Changed rose-gold to rose

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends React.Component {
     } else if (buttonClicked % 2 === 0) {
       return "Night-Theme";
     } else {
-      return "Rose-Gold";
+      return "Rose";
     }
   };
 
