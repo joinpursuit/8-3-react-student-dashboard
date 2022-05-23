271775b5ba3117f7d3669efaa917db6db06f4015
Jonathan Scheiber <jon@scheiber.me>
2022-05-22 23:56:44 -0400

    update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import data from "./data/data.json";
 const App = () => {
   const [list, setList] = useState(data);
   const [cohortName, setTitle] = useState("All Students");
+
+  return (
+    <div className="App">
+      <h1 className="title">
+        <a href="./">Student Dashboard</a>
+      </h1>
+      <StudentList data={list} title={cohortName} />
+      <SideNav data={data} setList={setList} setTitle={setTitle} />
+    </div>
+  );
 };
 
 export default App;
