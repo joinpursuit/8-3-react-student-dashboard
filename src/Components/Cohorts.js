c28b186042f304a75b76a3c8a43c62d12ca83553
Andres Varon <anvaron@gmail.com>
2022-05-24 12:33:36 -0400

    cohorts validation issue - fixed

--- src/Components/Cohorts.js
+++ src/Components/Cohorts.js
@@ -1,43 +1,36 @@
 import React from "react";
 
-class Cohorts extends React.Component {
-  constructor(props) {
-    super(props);
-    
-  }
+  function Cohorts(props) {
+    const { cohorts, getCohort } = props;
 
-  render() {
-    // >> Getting the cohorts data 
-    // >> The cohorts are organized by date, either in ascending or descending order. "All Students" should remain at the top.
-    let cohortListSorted = [];
-    
-    const filteredList = new Set(
-      (this.props.cohorts).map(e => e.cohort['cohortCode'])
+    let cohortsSorted = [];
+
+    // >> Creating a new list with a cohort unique code-identifiers 
+    const uniqueCohortList = new Set(
+      (cohorts).map(e => e.cohort['cohortCode'])
     )
-    const cohortList = ['*'].concat(Array.from(filteredList));
-    cohortList.map((cohort) => {
-      // >> Creating a new array with objects {semester: value, year: value}
-      cohortListSorted.push({['semester']: cohort.slice(0, -4), ['year']: cohort.slice(-4)});
-    })
-
-    cohortListSorted.sort((a, b) => {return a.year - b.year;});
+    const cohortList = ['*'].concat(Array.from(uniqueCohortList));
+    cohortList.map((cohort) => cohortsSorted.push({['semester']: cohort.slice(0, -4), ['year']: cohort.slice(-4)}))
+    
+    // >> Sorting the cohort list
+    cohortsSorted.sort((a, b) => {return a.year - b.year;});
 
     return(
       <section>
         <h2>Choose a class by Start Date</h2>
         <ul>
-          {cohortListSorted.map((cohort, index) => {
+          {cohortsSorted.map((cohort, index) => {
+            // >> Validating the cohort values
             if(cohort.year === '*') {
-              return <li key={index} onClick={() => this.props.getCohort('*')}><i className="fa fa-university"></i>  All Students</li> 
+              return <li key={index} onClick={() => getCohort('*')}><i className="fa fa-university"></i>  All Students</li> 
             }
             else {
-              return <li key={index} onClick={() => this.props.getCohort(`${cohort.semester}${cohort.year}`)}><i className="fa fa-university"></i>  {`${cohort.semester} ${cohort.year}`}</li>
+              return <li key={index} onClick={() => getCohort(`${cohort.semester}${cohort.year}`)}><i className="fa fa-university"></i>  {`${cohort.semester} ${cohort.year}`}</li>
             }
           })}
         </ul>
       </section>
     )
-  }
 }
 
 export default Cohorts;
