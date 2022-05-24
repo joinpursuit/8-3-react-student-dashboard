97ba1a4015147c8c87bd2be1b849e669af187ebe
Andres Varon <anvaron@gmail.com>
2022-05-24 13:48:22 -0400

    student certifications validation issue - fixed

--- src/Components/Students.js
+++ src/Components/Students.js
@@ -2,34 +2,24 @@ import React from "react";
 import ShowDetailsButton from "./ShowDetailsButton";
 import StudentStatus from "./StudentStatus";
 
-class Students extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showDetails: false,
-      studentStatus: '',
-    }
-  }
 
-  /**
-   * validateStudentStatus: validates the student certifications
-   * @param {String} resume -A student date of birth.
-   * @returns A human readable student date of bith (e.g) February 9, 1979
-   */
-   validateStudentStatus = (resume, linkedin, mockInterview, github, codewars) => {
-    if(resume && linkedin && mockInterview && github && (codewars > 600)) {
-      this.setState({ 
-        studentStatus: 'On Track to Graduate',
-      });
-    }
-  }
 
-  /**
-   * getFormatedDOB: generates a formated students date of birth
-   * @param {String} dob -A student date of birth.
-   * @returns A human readable student date of bith (e.g) February 9, 1979
-   */
-  getFormatedDOB = (dob) => {
+/**
+ * validateStudentStatus: validates the student certifications
+ * @param {String} resume -A student date of birth.
+ * @returns A human readable student date of bith (e.g) February 9, 1979
+ */
+const validateStudentStatus = (resume, linkedin, mockInterview, github, codewars) => {
+  return (resume && linkedin && mockInterview && github && (codewars > 600)) ? 'On Track to Graduate' : '';
+  
+}
+
+/**
+ * getFormatedDOB: generates a formated students date of birth
+ * @param {String} dob -A student date of birth.
+ * @returns A human readable student date of bith (e.g) February 9, 1979
+ */
+const getFormatedDOB = (dob) => {
     const date = new Date(dob);
     const year = date.getFullYear();
     const month = date.toLocaleString('default', { month: 'long' });
@@ -38,39 +28,39 @@ class Students extends React.Component {
     return `${month} ${day}, ${year}`; 
   }
 
-  render() {
-    const studentList = (this.props.students).map(e => e)
+function Students(props) {
+  const { students, filter } = props;
+  // >> 
+  const studentList = (students).map(e => e)
     
-    return(
-      <>
-        <ul>
-        {(studentList).map(student => {
-         return <li key={student.id}>
-                  <section className="personal-data">
-                    <img src={student.profilePhoto}/>
-                    <div>
-                      <div className="container">
-                        <h4>{`${student.names['preferredName']} ${(student.names['middleName']).substring(0, 1)} ${student.names['surname']}`}</h4>
-                        <span className="track-check">
-                          {/* {this.validateStudentStatus(student.certifications.resume,
-                                          student.certifications.linkedin,
-                                          student.certifications.mockInterview,
-                                          student.certifications.github,
-                                          student.codewars.current.total)} */}
-                          
-                        </span>
-                      </div>
-                      <p>Username: <span>{student.username}</span></p>
-                      <p>Birthday: <span>{this.getFormatedDOB(student.dob)}</span></p>
+  return(
+    <>
+      <ul>
+      {(studentList).map(student => {
+        return <li key={student.id}>
+                <section className="personal-data">
+                  <img src={student.profilePhoto}/>
+                  <div>
+                    <div className="container">
+                      <h4>{`${student.names['preferredName']} ${(student.names['middleName']).substring(0, 1)} ${student.names['surname']}`}</h4>
+                      <span className="track-check">
+                        {validateStudentStatus(student.certifications.resume,
+                                        student.certifications.linkedin,
+                                        student.certifications.mockInterview,
+                                        student.certifications.github,
+                                        student.codewars.current.total)}
+                      </span>
                     </div>
-                  </section> 
-                  <ShowDetailsButton studentList={studentList} studentId={student.id}/>  
-                </li>
-        })}
-        </ul>
-      </>
-    )
-  }
+                    <p>Username: <span>{student.username}</span></p>
+                    <p>Birthday: <span>{getFormatedDOB(student.dob)}</span></p>
+                  </div>
+                </section> 
+                <ShowDetailsButton studentList={studentList} studentId={student.id}/>  
+              </li>
+      })}
+      </ul>
+    </>
+  )
 }
 
 export default Students;
--- src/Components/StudentsByCohort.js
+++ src/Components/StudentsByCohort.js
@@ -7,33 +7,32 @@ import React from "react";
  * @returns update the students list by cohort
  */
 const validateCohort = (students, filter) => {
-    let cohort = '';
-    // >> Validating the cohort code 
-    if(filter !== '*') {
-      const cohortList = new Set(
-        (students).map(student => student.cohort['cohortCode'])
-      ) 
-      // >> Formating the cohort identifier in a human readable way
-      cohort = [...cohortList].find(e => e === filter);
-      cohort = `${cohort.slice(0, -4)}  ${cohort.slice(-4)}`
-    } else {
-      cohort = 'All Students';
-    }
-    return cohort;
+  let cohort = '';
+  // >> Validating the cohort code 
+  if(filter !== '*') {
+    const cohortList = new Set(
+      (students).map(student => student.cohort['cohortCode'])
+    ) 
+    // >> Formating the cohort identifier in a human readable way
+    cohort = [...cohortList].find(e => e === filter);
+    cohort = `${cohort.slice(0, -4)}  ${cohort.slice(-4)}`
+  } else {
+    cohort = 'All Students';
   }
+  return cohort;
+}
 
 function StudentsByCohort(props) {
   const { students, filter } = props;  
   
-    const totalStudents = (students).map(student => student.id);
+  const totalStudents = (students).map(student => student.id);
 
-    return(
-      <section className="list-heading">
-        <h2>{validateCohort(students, filter)}</h2>
-        <p>Total students: {(totalStudents).length}</p>
-      </section>
-    )
-  
+  return(
+    <section className="list-heading">
+      <h2>{validateCohort(students, filter)}</h2>
+      <p>Total students: {(totalStudents).length}</p>
+    </section>
+  )
 }
 
 export default StudentsByCohort;
