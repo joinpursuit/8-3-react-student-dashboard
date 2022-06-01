37237ba9ae6438a136b1cbcadc29c23a8c4191b5
Charlie Moran <Carlosamoran18@gmail.com>
2022-06-01 19:32:30 -0400

    how many?

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ import "./app.css"
 class App extends React.Component {
   constructor(){
     super();
-    render()
+    this.state = {
+      studentFilter: [...studentData],
+      cohort: "All Students",
+    }
   }
+  classFilter = (date) => {
+    const classSelect = studentData.filter( (student) => student.cohort.cohortCode === date);
+    const howMany = date === "AllStudents" ? [...studentData] : classSelect;
+    this.setState({
+      studentFilter: howMany,
+      cohort: date,
+    })
+  };
+  render()
 }
