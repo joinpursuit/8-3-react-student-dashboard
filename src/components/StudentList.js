8e73141af37899e56668c5b1f23add73d4e51b93
Charlie Moran <Carlosamoran18@gmail.com>
2022-06-01 23:50:51 -0400

    student list edited

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,69 +1,96 @@
 import React from "react";
-import Student from "./Student";
+import Show from "./Show.js";
+
+const Graduating = (student) => {
+     const certifications = Object.values(student.certifications);
+     const codeWars = student.codewars.current.total;
+     if (codeWars > 600 && !certifications.includes(false)) {
+         return true;
+     } else {
+         return false;
+     }
+};
 
 class StudentList extends React.Component {
-    selectCohort = (input, selectedCohort) => {
-        if(selectedCohort === "AllStudents") {
-            return input;
-        } else {
-            return input.filter((student) => {
-                return student.cohort.cohortCode === selectedCohort;
-            })
+    constructor() {
+        super()
+        this.state = {
+            show: false,
+            person: "",
+            comment: "",
+            newSubmission: [],
         }
     };
 
-    displayStudents = (
-        input,
-        selectedCohort,
-        deets,
-        deetsNstuff,
-        comments,
-        commentStuff,
-     ) => {
-        let selectedStudent = this.selectCohort(input, selectedCohort);
-
-        return selectedStudent.map((selectedStudent, index) => {
-            return (
-                <Student studentInfo={selectedStudent} key={index} 
-                         deets={deets} deetsNstuff={deetsNstuff}
-                         comments={comments} commentStuff={commentStuff} 
-                />
-            )
-        })
+    showMore = () => {
+        this.setState({show: !this.state.show,})
     };
 
-    parseCohort = (cohort) => {
-        if(cohort === "AllStudents") return "All Students";
-        return `${cohort.substring(0, cohort.length -4)} ${cohort.substring(cohort.length -4)}`
+    personStuff = (evt) => {
+        const { value } = evt.target;
+        this.setState({person:value,})
+    };
+
+    commentStuff = (evt) => {
+        const { value } = evt.target;
+        this.setState({comment:value,})
+    };
+
+    submitting = (evt) => {
+        evt.preventDefault();
+        const submission = `${this.state.person} says "${this.state.comment}"`;
+        this.setState({newSubmission:[...this.state.newSubmission, submission]});
+        console.log(this.state.newSubmission);
     };
 
     render() {
-        const {
-            input,
-            selectedCohort,
-            deets,
-            deetsNstuff,
-            comments,
-            commentStuff,
-        } = this.props
-
-        const studentCards = this.displayStudents(
-            input,
-            selectedCohort,
-            deets,
-            deetsNstuff,
-            comments,
-            commentStuff,
-        )
+        const { student } = this.props;
+        const { names, dob, profilePhoto, username } = student;
+        const { show, person, comment, newSubmission } = this.state;
+        const dobFormat = new Date(dob).toDateString().substring(4);
+        const { preferredName, middleName, surname } = names;
         return (
-            <div className="studentList">
-                <h2>{this.parseCohort(selectedCohort)}</h2>
-                <h3 className="studentCard">Total Students: {studentCards.length}</h3>
-                {studentCards}
-                <Student />
+          <div className="card">
+            <img
+              className="pic"
+              alt={preferredName}
+              src={profilePhoto}
+              width={120}
+              height={120}
+            />
+            <section className="info">
+              <div className="name">
+                {preferredName} {middleName[0]}. {surname}
+              </div>
+              <div className="other">
+                {username} 📧
+                <br />
+                Birthday: {dobFormatted}
+                <br />
+                <br />
+                <div onClick={() => this.showMore()}>
+                  {show ? "Show Less..." : "Show More..."}
+                </div>
+                {show ? (
+                  <Show
+                    student={student}
+                    showMore={this.showMore}
+                    person={person}
+                    comment={comment}
+                    personStuff={this.personStuff}
+                    commentStuff={this.commentStuff}
+                    submitting={this.submitting}
+                    newSubmission={newSubmission}
+                  />
+                ) : null}
+              </div>
+            </section>
+            <div className="track">
+              {Graduating(student) ? "On Track To Graduate" : null}
             </div>
-        )
+          </div>
+        );
+      }
     }
-}
-
-export default StudentList;
+    
+    export default StudentDirectory;
