04f43d071a69108c1bfe3796752c7f1ebb4af3de
Isaac Gonzalez <u1337x@gmail.com>
2022-06-09 14:56:19 -0400

    On track

--- src/components/oneStudent.js
+++ src/components/oneStudent.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ShowMore from "./showMore";
 
+function graduate(student) {
+  const { certifications } = student;
+  if (
+    certifications.resume &&
+    certifications.linkedin &&
+    certifications.github &&
+    certifications.mockInterview
+  ) {
+    return <p>On track to graduate.</p>;
+  }
+}
+
 function oneStudent({ student }) {
   const { names, username, profilePhoto, dob } = student;
   return (
@@ -9,6 +21,7 @@ function oneStudent({ student }) {
       <p>{username}</p>
       <p>Birthday: {dob}</p>
       <img src={profilePhoto} alt={names.preferredName} />
+      {graduate(student)}
       <ShowMore student={student} />
     </article>
   );
